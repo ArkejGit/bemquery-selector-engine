f56e2c9b6be13d4fd7d4446a3eb7e2350edf3ce4
ArkejGit <robert.krawczyk92@gmail.com>
2017-12-03 18:42:57 +0100

    Add SelectorEngine

---
+++ src/SelectorEngine.js
@@ -0,0 +1,35 @@
+'use strict';
+
+class SelectorEngine {
+	constructor( converter ) {
+		this.converter = converter;
+	}
+
+	find( selector, context = document ) {
+		const convertedSelector = this.converter.convert( selector );
+		const result = {
+			selector: convertedSelector
+		};
+		let cssSelector = convertedSelector.CSS;
+		let tmpId = false;
+
+		if ( context !== document ) {
+			if ( !context.id ) {
+				tmpId = true;
+				context.id = `BEMQueryTMP_${Date.now()}`;
+			}
+
+			cssSelector = `#${context.id} ${cssSelector}`;
+		}
+
+		result.elements = Array.from( context.querySelectorAll( cssSelector ) );
+
+		if ( tmpId ) {
+			context.removeAttribute( 'id' );
+		}
+
+		return result;
+	}
+}
+
+export default SelectorEngine;
