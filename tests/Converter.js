800cb091075b495b3f4dff902186875c6d2a3d21
ArkejGit <robert.krawczyk92@gmail.com>
2017-12-03 17:03:43 +0100

    Fix wrong tests and remove console.log

--- tests/Converter.js
+++ tests/Converter.js
@@ -36,16 +36,15 @@ describe ( 'Converter', () => {
 			'block:modifier elem:modifier': '.block_modifier .block__elem_modifier',
 			'block elem elem': '.block__elem__elem',
 			'block elem elem:modifier': '.block__elem__elem_modifier',
-			'block:modifier elem elem': '.block_modifier .block__elem_elem',
+			'block:modifier elem elem': '.block_modifier .block__elem__elem',
 			'block elem:modifier elem': '.block__elem_modifier .block__elem__elem',
-			'block:modifier elem:modifier elem': '.block_modifier .block__elem_modifier .block_elem_elem'
+			'block:modifier elem:modifier elem': '.block_modifier .block__elem_modifier .block__elem__elem'
 		};
 
 		const converter = new Converter();
 
 		Object.keys( selectors ).forEach( ( selector ) => {
 			const result = converter.convert( selector );
-			console.log( result );
 			expect( result ).to.be.an.instanceof( Selector );
 			expect( result.BEM ).to.equal( selector );
 			expect( result.CSS ).to.equal( selectors[ selector ] );
